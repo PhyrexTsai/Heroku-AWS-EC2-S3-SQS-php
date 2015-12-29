d8530b58fe42b27c6eae7873a010f40dc1d3fe21
PhyrexTsai <x01ep23i@hotmail.com>
2015-12-29 09:59:26 +0800

    go

--- web/sqsSend.php
+++ web/sqsSend.php
@@ -4,23 +4,26 @@ require('config.php');
 require('../vendor/autoload.php');
 use Aws\Sqs\SqsClient;
 
-$sqs = new SqsClient([
-    'version' => SQS_VERSION,
-    'region'  => SQS_REGION
-]);
-
-$sqs->sendMessage(array(
-    'QueueUrl'      => SQS_INBOX,
-    'MessageBody'   => 'Submit a message to Thumbnail image',
-    'MessageAttributes' => array(
-        // Associative array of custom 'String' key names
-        'thumbnail' => array(
-            'StringValue' => 'image_name',
-            // DataType is required
-            'DataType' => 'String',
+function send() {
+    $sqs = new SqsClient([
+        'version' => SQS_VERSION,
+        'region'  => SQS_REGION
+    ]);
+    
+    $result = $sqs->sendMessage(array(
+        'QueueUrl'      => SQS_INBOX,
+        'MessageBody'   => 'Submit a message to Thumbnail image',
+        'MessageAttributes' => array(
+            // Associative array of custom 'String' key names
+            'thumbnail' => array(
+                'StringValue' => 'image_name',
+                // DataType is required
+                'DataType' => 'String',
+            ),
+            // ... repeated
         ),
-        // ... repeated
-    ),
-));
-
+    ));
+    
+    print_r($result);
+}
 ?>
